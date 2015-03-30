d229c9384d228be1854f80756b28b866c3e06c6b
Jonathan Barratt <jonathan.barratt@the-im.com>
2015-03-30 15:33:02 +0700

    Updated setup scripts and documentation to support boot2docker, the official Docker-for-OSX tool
    -Only prefix commands with sudo if $OSTYPE shows we're running under darwin
    -Also fix some minor typos

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
+if [[ $OSTYPE != darwin* ]]; then
+  SUDO=sudo
+fi
+
 sudo mkdir -p /data/postgresql
 
-sudo docker run -d --name postgres    -p 5432:5432  -v /data/postgresql:/var/lib/postgresql/data postgres
-sudo docker run -d --name taiga-back  -p 8001:8001  --link postgres:postgres ipedrazas/taiga-back
-sudo docker run -d --name taiga-front -p 80:80 -p 8000:8000 --link taiga-back:taiga-back ipedrazas/taiga-front
+$SUDO docker run -d --name postgres    -p 5432:5432  -v /data/postgresql:/var/lib/postgresql/data postgres
+$SUDO docker run -d --name taiga-back  -p 8001:8001  --link postgres:postgres ipedrazas/taiga-back
+$SUDO docker run -d --name taiga-front -p 80:80 -p 8000:8000 --link taiga-back:taiga-back ipedrazas/taiga-front
 
 
-sudo docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
-sudo docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createdb -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -O taiga taiga'";
-sudo docker run -it --rm --link postgres:postgres ipedrazas/taiga-back bash regenerate.sh
+$SUDO docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
+$SUDO docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createdb -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -O taiga taiga'";
+$SUDO docker run -it --rm --link postgres:postgres ipedrazas/taiga-back bash regenerate.sh
 
 
-sudo docker stop taiga-front
-sudo docker stop taiga-back
-sudo docker stop postgres
+$SUDO docker stop taiga-front
+$SUDO docker stop taiga-back
+$SUDO docker stop postgres
