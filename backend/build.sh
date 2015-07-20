42676365b7f113f00ab4ea02e6a24f668262316c
Ivan Pedrazas <ipedrazas@gmail.com>
2015-07-20 23:22:22 +0100

    move build to stable branch

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,8 @@ if [[ -d taiga-back ]]; then
     rm -rf taiga-back
 fi
 
-# git clone -b stable --single-branch https://github.com/taigaio/taiga-back.git
-git clone https://github.com/taigaio/taiga-back.git
+git clone -b stable --single-branch https://github.com/taigaio/taiga-back.git
+# git clone https://github.com/taigaio/taiga-back.git
 
 sed -i '.bak' 's/^enum34/#enum34/' taiga-back/requirements.txt
 cp taiga-back/requirements.txt .
