47cf4f0b595194051eef88e3c4abaf006c69a0a3
Ivan Pedrazas <ipedrazas@gmail.com>
2015-07-22 23:01:30 +0100

    sleep added to give some air to postgres

--- setup.sh
+++ setup.sh
@@ -13,9 +13,10 @@ sudo mkdir -p /data/postgres
 
 
 $SUDO docker run -d --name postgres  -v /data/postgres:/var/lib/postgresql/data postgres
+# postgres needs some time to startup
+sleep 5
 $SUDO docker run -d --name taiga-back  -p 8000:8000  --link postgres:postgres ipedrazas/taiga-back
 $SUDO docker run -d --name taiga-front -p 80:80 -e API_NAME=$API_NAME --link taiga-back:taiga-back ipedrazas/taiga-front
-
 
 $SUDO docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
 $SUDO docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createdb -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -O taiga taiga'";
