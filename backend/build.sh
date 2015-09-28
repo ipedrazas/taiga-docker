8253664edc9d9a56afb5beb4be77ebe10e39258c
Ivan Pedrazas <ipedrazas@gmail.com>
2015-09-28 23:05:39 +0100

    fix issue #18

--- backend/build.sh
+++ backend/build.sh
@@ -11,9 +11,11 @@ git clone -b stable --single-branch https://github.com/taigaio/taiga-back.git
 if [[ $OSTYPE != darwin* ]]; then
     sed -i 's/^enum34/#enum34/' taiga-back/requirements.txt
     sed -i -e '/sample_data/s/^/#/' taiga-back/regenerate.sh
+    sed -i -e 's/django.db.backends.postgresql_psycopg2/transaction_hooks.backends.postgresql_psycopg2/g' settings/local.py
 else
     sed -i '.bak' 's/^enum34/#enum34/' taiga-back/requirements.txt
     sed -i '.bak' '/sample_data/s/^/#/' taiga-back/regenerate.sh
+    sed -i 's/django.db.backends.postgresql_psycopg2/transaction_hooks.backends.postgresql_psycopg2/g' settings/local.py
 fi
 
 cp taiga-back/requirements.txt .
