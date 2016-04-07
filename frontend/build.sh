8e2fae6c2eec8c47e839dbe01cd190cdf96a0812
Matteo Ferraroni <matteo.ferraroni@gmail.com>
2016-04-07 20:39:18 +0000

    Fix conf.json location

--- frontend/build.sh
+++ frontend/build.sh
@@ -6,6 +6,9 @@ fi
 
 git clone https://github.com/taigaio/taiga-front-dist
 
+# Production ready frontend is in "stable" and not in "master" therefore after clone I need to change to "stable"
+git checkout stable
+
 $SUDO docker build -t ipedrazas/taiga-front .
 
 if [[ -d  taiga-front-dist ]]; then
--- frontend/run.sh
+++ frontend/run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-sed -i "s/API_SERVER/$API_NAME/g" /taiga/js/conf.json
+sed -i "s/API_SERVER/$API_NAME/g" /taiga/conf.json
 
 nginx -g "daemon off;"
