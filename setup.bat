0255ff903cb9171c18168af78fd4e6d3e0372249
Mohamed Mellouki <mellouki.mhd@gmail.com>
2017-04-02 13:04:44 -0400

    Windows batch setup support
    Add windows batch setup file based on the shell script

---
+++ setup.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+set api_name=%API_NAME%
+
+IF "%api_name%" == "" (
+    set API_NAME="localhost";
+)
+
+echo API_NAME: $API_NAME
+
+mkdir -p /data/postgres
+
+docker run -d --name postgres  -v /data/postgres:/var/lib/postgresql/data postgres
+:: postgres needs some time to startup
+sleep 5
+docker run -d --name taiga-back  -p 8000:8000 -e API_NAME=$API_NAME  --link postgres:postgres ipedrazas/taiga-back
+docker run -d --name taiga-front -p 80:80 -e API_NAME=$API_NAME --link taiga-back:taiga-back --volumes-from taiga-back ipedrazas/taiga-front
+
+docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
+docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createdb -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -O taiga taiga'";
+docker run -it --rm --link postgres:postgres ipedrazas/taiga-back bash regenerate.sh
+
+
+
+
+
+
+
